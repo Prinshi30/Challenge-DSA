7cdceb59ef229e9713d89dc08671d2cada38a00e
Priyanshi singh <95485651+Prinshi30@users.noreply.github.com>
2024-11-11 13:48:38 +0530

    Update twoSum.java

--- 75 leetcode/twoSum.java
+++ 75 leetcode/twoSum.java
@@ -3,10 +3,10 @@ class Solution {
         HashMap<Integer,Integer> map=new HashMap<>(); // create hashmap
         for(int i=0;i<nums.length;i++){ // loop
             int diff = target-nums[i]; 
-            if(map.containKey(diff)){
+            if(map.containsKey(diff)){
                 return new int[]{map.get(diff),i}; // here we check value contain or not
             }
-            map.put(num[i],i);        
+            map.put(nums[i],i);        
             }
             return null;
     }
