a46eb165027258ef8148501a447f22658fb31587
Priyanshi singh <95485651+Prinshi30@users.noreply.github.com>
2024-11-11 13:42:42 +0530

    Update twoSum.java

--- 75 leetcode/twoSum.java
+++ 75 leetcode/twoSum.java
@@ -4,7 +4,7 @@ class Solution {
         for(int i=0;i<nums.length;i++){ // loop
             int diff = target-nums[i]; 
             if(map.containKey(diff)){
-                return new int[]){map.get(diff),i}; // here we check value contain or not
+                return new int[]{map.get(diff),i}; // here we check value contain or not
             }
             map.put(num[i],i);        
             }
