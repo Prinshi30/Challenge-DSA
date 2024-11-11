c40bc34981debeade1f289613ac0a4a0404c705a
Priyanshi singh <95485651+Prinshi30@users.noreply.github.com>
2024-11-11 14:46:02 +0530

    Create contain dublicate.java

---
+++ 75 leetcode/contain dublicate.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+       Set<Integer> visited =new HashSet<>(); // create hashSet
+       for(int i=0;i<nums.length();i++){
+            if(visited.cotains(nums[i])) { // check exist or not 
+                return true;
+            }else{
+                visited.add(nums[i]); // add is not exist
+            }
+       }
+
+        return false; // if not found still false
+    }
+}
