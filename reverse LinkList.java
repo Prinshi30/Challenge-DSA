8d6502709978f0b6a79a127dfb2d0f530c7e2383
Priyanshi singh <95485651+Prinshi30@users.noreply.github.com>
2024-11-13 14:16:59 +0530

    Update reverse LinkList.java

--- reverse LinkList.java
+++ reverse LinkList.java
@@ -10,14 +10,14 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        ListNode pev=null;
-        ListNode curr= head;
-        while(curr!= null){
-            ListNode next = curr.next;
-            curr.next=pev;
-            pev=curr;
+     ListNode prev=null;
+    ListNode curr=head;
+        while(curr != null){
+            ListNode next= curr.next;
+            curr.next=prev;
+            prev= curr;
             curr=next;
         }
-        return pev;
+        retrun prev;
     }
 }
